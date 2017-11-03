d01d5388bcfa9193dfa6b446df6612480632076c
isaque alves de lima <isaquealvesdl@gmail.com>
2017-11-03 10:29:41 -0200

    Applying system logging techniques in Frame and SDLSystem class of engine

--- include/Engine/Frame.hpp
+++ include/Engine/Frame.hpp
@@ -10,13 +10,15 @@
 #include "Engine/sdl2include.hpp"
 
 class Frame {
-public:
-    Frame(int positionX, int positionY, int width, int height);
-    SDL_Rect *GetRect();
+	public:
+	    Frame(int positionX, int positionY, int width, int height);
+	    SDL_Rect *GetRect();
 
-private:
-    // Rectangular area of the frame
-    SDL_Rect sourceRectangle = {0, 0, 0, 0};
+	private:
+	    // Rectangular area of the frame
+	    SDL_Rect sourceRectangle = {
+	    	0, 0, 0, 0
+	    };
 };
 
-#endif // __FRAME__
+#endif
